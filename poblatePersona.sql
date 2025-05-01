3d92bbc227fa52a15aee57a0f76211cd62055170
aroaaredondo <124912378+aroaaredondo@users.noreply.github.com>
2025-05-01 17:14:03 +0200

    Update poblatePersona.sql

--- poblatePersona.sql
+++ poblatePersona.sql
@@ -1,2 +1,7 @@
 INSERT INTO "persona" ("id", "nombre", "email", "rol", "rol_secundario", "departamento", "password") VALUES
-(1, 'Juan Pérez', 'juan@gmail.com', 'ESTUDIANTE', NULL, NULL, '$2a$10$a6mMmmQJMTyAvNpxfZhBnea.1mgUgXzmoXF4u4Lu1llN0sXsUNRXa')
+(1, 'Juan Pérez', 'juan@gmail.com', 'ESTUDIANTE', NULL, NULL, '$2a$10$a6mMmmQJMTyAvNpxfZhBnea.1mgUgXzmoXF4u4Lu1llN0sXsUNRXa'),
+(2, 'María García', 'maria@gmail.com', 'INVESTIGADOR_CONTRATADO', NULL, NULL,'$2a$10$i3D5Epvq6YGayBazaEcpWuxf/79IMR3se7XQfVea1HwdzeneohO4i'),
+(3, 'Carlos López', 'carlos@gmail.com', 'DOCENTE_INVESTIGADOR', NULL, NULL,'$2a$10$K5aRwfc7/I22IrIkQN1IPemhhoJ2KUfYyAIALRLlEbjE5GRpSeTyO'),
+(4, 'Ana Torres', 'ana@gmail.com', 'CONSERJE', NULL, NULL,'$2a$10$i3j8PH752uUH42WFwyGK3.PUE6rvWIetKTy47aQ3ZI4djYAoI15v6'),
+(5, 'Luis Fernández', 'luis@gmail.com', 'TECNICO_LABORATORIO', NULL, NULL,'$2a$10$TIOxloCaXW5sKazIizUa6upp2nFVzbKDqZohYJuC7W8Jw6c8YQglG'),
+(6, 'Laura González', 'laura@gmail.com', 'GERENTE', NULL, NULL,'$2a$10$MeTViS4mVhc1Jeh2Y5pNAeqevEQDcpgJzAPpMHiAOo/7/WhVfJT62');
